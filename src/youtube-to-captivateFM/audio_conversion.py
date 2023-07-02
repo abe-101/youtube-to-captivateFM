6ccb472a378a2ccd6589dc229097fa14972322a7
Abe <abe@habet.dev>
2023-07-01 23:44:16 -0400

    fix video conversion

--- src/youtube-to-captivateFM/audio_conversion.py
+++ src/youtube-to-captivateFM/audio_conversion.py
@@ -1,4 +1,6 @@
 import subprocess
+from moviepy.editor import AudioFileClip, ImageClip
+
 from typing import Optional
 
 from pydub import AudioSegment
@@ -19,40 +21,23 @@ def normalize_volume(file_path: str, target_dBFS: float = -15.0) -> str:
     return file_path
 
 
-
-def create_video_from_audio_and_picture(audio_file: str, image_file: str, video_file: str) -> str:
-    """
-    Creates a video from an audio file and an image file, and saves the video as a new file.
-
-    Args:
-        audio_file (str): The path to the audio file.
-        image_file (str): The path to the image file.
-        video_file (str): The path to save the video file.
-
-    Returns:
-        str: The name of the created video file.
-
-    Raises:
-        ValueError: If either the audio or image file does not exist.
-
-    """
-
-    ffmpeg_command = [
-        "ffmpeg",
-        "-loop",
-        "1",
-        "-y",  # Overwrite output file without asking
-        "-i",
-        image_file,
-        "-i",
-        audio_file,
-        "-shortest",
-        video_file,
-    ]
-    subprocess.run(ffmpeg_command)
-
-
-    return video_file
+def create_video_from_audio_and_picture(audio_path: str, image_path: str, output_path: str) -> str:
+    """Create and save a video file to `output_path` after
+    combining a static image that is located in `image_path`
+    with an audio file in `audio_path`"""
+    # create the audio clip object
+    audio_clip = AudioFileClip(audio_path)
+    # create the image clip object
+    image_clip = ImageClip(image_path)
+    # use set_audio method from image clip to combine the audio with the image
+    video_clip = image_clip.set_audio(audio_clip)
+    # specify the duration of the new clip to be the duration of the audio clip
+    video_clip.duration = audio_clip.duration
+    # set the FPS to 1
+    video_clip.fps = 1
+    # write the resuling video clip
+    video_clip.write_videofile(output_path)
+    return output_path
 
 
 def convert_wav_to_mp3(input_file: str, output_file: Optional[str] = None) -> str:
