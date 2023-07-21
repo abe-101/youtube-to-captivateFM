f898ea09cc65548516f38b30e19c7e5b50c638c5
Abe <abe@habet.dev>
2023-07-21 12:27:59 -0400

    add rm

---
+++ src/youtube-to-captivateFM/rm_fetch.py
@@ -0,0 +1,72 @@
+import csv
+import re
+
+import feedparser
+from configuration_manager import ConfigurationManager
+from dotenv import load_dotenv
+from generate_post import Video, get_most_recent_videos_from_playlist
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+import googleapiclient.errors
+
+
+
+import yt_dlp
+
+load_dotenv()
+config = ConfigurationManager()
+api_key = config.YOUTUBE_API
+
+
+def get_new_videos(show):
+    playlist_id = show["playlist_id"]
+    
+    max_results = 49
+
+    youtube = build("youtube", "v3", developerKey=api_key)
+    # Create a YouTube API service object
+    
+    db = get_most_recent_videos_from_playlist(youtube, playlist_id, max_results)
+
+    #playlist_url = f"https://www.youtube.com/playlist?list={show['playlist_id']}"
+    #ydl_opts = {"dump_single_json": True, "extract_flat": True, "format": "best"}
+    #
+    #with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    #    playlist_data = ydl.extract_info(playlist_url, download=False)
+    
+    podcast_ids = set()
+    podcast_titles = set()
+    
+    url = "https://feeds.captivate.fm/" + show["rss"]
+    feed = feedparser.parse(url)
+    print(len(feed["entries"]))
+
+    for entry in feed["entries"]:
+        youutbe_id = find_youtube_video_id(entry["summary"])
+        if youutbe_id:
+            podcast_ids.add(youutbe_id)
+        podcast_titles.add(entry["title"])
+    print(f"Podcast ids: {len(podcast_ids)}")
+    print(f"Podcast titles: {len(podcast_titles)}")
+
+
+    youtube = []
+    for video in db:
+        if video.id not in podcast_ids and video.name not in podcast_titles:
+            youtube.append((video.id, video.name))
+    
+    print(f"Youtube: {len(youtube)}")
+    return youtube
+
+
+            
+
+def find_youtube_video_id(text):
+    pattern = r"(?:youtube\.com\/watch\?v=|youtu.be\/)([A-Za-z0-9_-]+)"
+    match = re.search(pattern, text)
+    if match:
+        video_id = match.group(1)
+        return video_id
+    else:
+        return None
+
---
+++ src/youtube-to-captivateFM/rm_minkowicz.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python
+
+import asyncio
+import time
+import re
+from datetime import datetime
+
+from dotenv import load_dotenv
+from imgurpython import ImgurClient
+
+from adobe_podcast import enhance_podcast
+from audio_conversion import (
+    combine_mp3_files,
+    combine_webm_files,
+    convert_wav_to_mp3,
+    create_video_from_audio_and_picture,
+)
+from captivate_api import create_podcast, format_date, get_episode, publish_podcast, update_podcast, upload_media
+from configuration_manager import ConfigurationManager, LocalMedia
+from download_yt import download_youtube_video
+from spotify import get_latest_spotify_episode_link
+from upload_video import upload_video_with_options
+from rm_fetch import get_new_videos
+import logging
+
+# Set up logging configuration
+logging.basicConfig(
+    filename="ry-minkowicz.log", level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+# Get a logger instance for the current module
+logger = logging.getLogger(__name__)
+
+
+load_dotenv()
+
+config = ConfigurationManager()
+
+def happy_playlist():
+    new_videos = get_new_videos(config.rm_happy)
+    n = len(new_videos)
+    count = 1
+    for id, title in new_videos:
+        print(f"Downloading({count}/{n}) - {title}")
+        happy_podcast(id)
+        count += 1
+
+def tanya_playlist():
+    new_videos = get_new_videos(config.rm_tanya)
+    for id, title in new_videos:
+        print(f"Downloading {title}")
+        tanya_podcast(id)
+
+def maamor_playlist():
+    new_videos = get_new_videos(config.rm_maamor)
+    n = len(new_videos)
+    count = 1
+    for id, title in new_videos:
+        print(f"Downloading({count}/{n}) - {title}")
+        maamor_podcast(id)
+        count += 1
+
+def torah_playlist():
+    new_videos = get_new_videos(config.rm_torah)
+    n = len(new_videos)
+    count = 1
+    for id, title in new_videos:
+        print(f"Downloading({count}/{n}) - {title}")
+        torah_podcast(id)
+        count += 1
+
+def maamor_podcast(url: str):
+    local_media: LocalMedia = download_youtube_video(url, config.rm_maamor["dir"], logger=logger)
+    episode = publish_podcast(local_media, config.rm_maamor, config)
+    print(episode)
+    
+def happy_podcast(url: str):
+    local_media: LocalMedia = download_youtube_video(url, config.rm_happy["dir"], logger=logger)
+    string = local_media.title
+    match = re.search(r"Day\s+(\d+)", string)
+
+    if match:
+        number = match.group(1)
+        print(number)
+    else:
+        number = 1
+    episode = publish_podcast(local_media, config.rm_happy, config, episode_num=number, logger=logger)
+    print(episode)
+
+def tanya_podcast(url: str):
+    local_media: LocalMedia = download_youtube_video(url, config.rm_tanya["dir"], logger=logger)
+
+    string = local_media.title
+    
+    # Use regular expression to find the number
+    match = re.search(r"\d+", string)
+    
+    if match:
+        number = match.group()
+        print(number)
+    else:
+        number = 1
+
+    episode = publish_podcast(local_media, config.rm_tanya, config, episode_num=number, logger=logger)
+    print(episode)
+
+def torah_podcast(url: str):
+    local_media: LocalMedia = download_youtube_video(url, config.rm_torah["dir"], logger=logger)
+    episode = publish_podcast(local_media, config.rm_torah, config, logger=logger)
+    print(episode)
+
+
